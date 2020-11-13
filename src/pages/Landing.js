22a25814a29eee13162fe0ace35f1609e6926ddf
vitor-risso <vitor.risso13@gmail.com>
2020-11-13 19:26:07 -0300

    organizaçao de pastas criada, criação de rotas feita, estilização global criada

---
+++ src/pages/Landing.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Header from '../components/Header/Header';
+
+import '../styles/pages/landing.css'
+
+function Landing(){
+    return(
+        <div className='landing-page'>
+            <Header />
+        </div>
+    )
+}
+
+export default Landing
