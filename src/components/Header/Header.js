6ce471d9a3997723733df690659ff93578fead2e
vitor-risso <vitor.risso13@gmail.com>
2020-11-13 21:05:45 -0300

    card adicionado, cor de fundo da di alterada, texto do rodapé estilizado, fala estilizar texto de cima

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,16 @@
 import React from 'react'; 
 
+import logo from '../../assets/img/Imagem3.png'
+
 import './index.css'
 
 function Header(){
     return(
         <header>
-
+            <div className='home'>
+                <img src={logo} alt='Logo' />
+                <h1>SuperGiantGames</h1>
+            </div>
         </header>
     )
 }
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -2,12 +2,22 @@ import React from 'react';
 
 import Header from '../components/Header/Header';
 
+import Card from '../assets/img/card.png'
+
 import '../styles/pages/landing.css'
 
 function Landing(){
     return(
         <div className='landing-page'>
             <Header />
+            <div className='card'>
+                <p><strong> TRANSISTOR - RED THE SINGER</strong></p>
+                <img src={Card} alt='Card de personagem' />
+                <div className='text'>
+                    <p>"Olha, o que quer que você esteja pensando, me faça um favor, não solte."</p>
+                </div>
+
+            </div>
         </div>
     )
 }
